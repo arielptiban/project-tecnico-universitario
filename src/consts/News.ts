6cc617c9e321a60a9e64feb57f62a0f0619c6cba
Ariel <user@MacBook-Pro-de-Ariel.local>
2025-01-16 13:22:33 -0500

    Add News Section and Video Section

---
+++ src/consts/News.ts
@@ -0,0 +1,41 @@
+import type { News } from "@/types/News";
+
+export const NEWS: News[] = [
+    {
+        id: "inicia-la-pretemporada",
+        fecha: "06 ENERO 2025",
+        titulo: "Inicia la pretemporada en Quillanloma",
+        descripcion: `
+        Técnico Universitario arrancó su pretemporada 2025 en el complejo de Quillanloma, con el objetivo de llegar en óptimas condiciones al inicio de la LigaPro el próximo 14 de febrero. El equipo ambateño ya trabaja bajo la dirección del cuerpo técnico, combinando sesiones físicas y tácticas para enfrentar los retos del campeonato.
+        La plantilla se fortalece con nuevos refuerzos y renovaciones estratégicas que buscan consolidar un grupo competitivo. Además, se programarán partidos amistosos en las próximas semanas para ajustar detalles antes del debut oficial.
+        La ilusión de los hinchas del "Rodillo Rojo" crece día a día, soñando con una temporada llena de éxitos que ponga en alto el nombre de Ambato. ¡Todo está listo para que el sueño 2025 comience a rodar!
+        `,
+        imagen: "/img/banner-2.webp",
+    },
+    {
+        id: "nueva-coleccion",
+        fecha: "15 ENERO 2025",
+        titulo: "La nueva colección Ambato cocido en mi pecho",
+        descripcion: "El nuevo estadio en Ambato será inaugurado con un emocionante partido amistoso.",
+        imagen: "/img/banner.webp",
+    },
+    {
+        id: "inicia-la-pretemporada",
+        fecha: "06 ENERO 2025",
+        titulo: "Inicia la pretemporada en Quillanloma",
+        descripcion: `
+        Técnico Universitario arrancó su pretemporada 2025 en el complejo de Quillanloma, con el objetivo de llegar en óptimas condiciones al inicio de la LigaPro el próximo 14 de febrero. El equipo ambateño ya trabaja bajo la dirección del cuerpo técnico, combinando sesiones físicas y tácticas para enfrentar los retos del campeonato.
+        La plantilla se fortalece con nuevos refuerzos y renovaciones estratégicas que buscan consolidar un grupo competitivo. Además, se programarán partidos amistosos en las próximas semanas para ajustar detalles antes del debut oficial.
+        La ilusión de los hinchas del "Rodillo Rojo" crece día a día, soñando con una temporada llena de éxitos que ponga en alto el nombre de Ambato. ¡Todo está listo para que el sueño 2025 comience a rodar!
+        `,
+        imagen: "/img/banner-2.webp",
+    },
+    {
+        id: "nueva-coleccion",
+        fecha: "15 ENERO 2025",
+        titulo: "La nueva colección Ambato cocido en mi pecho",
+        descripcion: "El nuevo estadio en Ambato será inaugurado con un emocionante partido amistoso.",
+        imagen: "/img/banner.webp",
+    },
+
+];
---
+++ src/types/News.ts
@@ -0,0 +1,7 @@
+export type News = {
+    id: string;
+    fecha: string; // Puedes usar ISO 8601 strings
+    titulo: string;
+    descripcion: string;
+    imagen: string;
+};
